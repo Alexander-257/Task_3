efb165b10d0f97aec0b99ddef355e9f60c46f99b
Александр Соколов <alexander.sokolov.a.s.257@gmail.com>
2021-11-05 13:48:11 +0300

    0.3

--- game.java
+++ game.java
@@ -15,10 +15,6 @@ import java.util.Scanner;
 public class game {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
-        String se = null;
-        for(int i = 0; i < args.length; i++) {
-            se += args[i] + " ";
-        }
         if(args.length < 1) {
             System.out.println("Objects are missing!");
         } else {
@@ -26,14 +22,6 @@ public class game {
                 System.out.println("The number of elements must be >= 3!");
             } else {
                 if (args.length >= 3 & args.length % 2 != 0) {
-                    try {
-                        SecureRandom random = new SecureRandom();
-                        Integer key = random.nextInt();
-                        byte[] hmacSha256 = game.calcHmacSha256(key.toString().getBytes("UTF-8"), se.getBytes("UTF-8"));
-                        System.out.println(String.format("HMAC: %032x", new BigInteger(1, hmacSha256)).toUpperCase(Locale.ROOT));
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
                     Menu(args);
                 } else  { System.out.println("An odd number of elements!"); }
             }
@@ -41,41 +29,56 @@ public class game {
     }
 
     public static void Menu(String[] arguments) throws NoSuchAlgorithmException, IOException {
-        System.out.println("\nAvailable moves:");
+        String se = null;
+        for(int i = 0; i < arguments.length; i++) {
+            se += arguments[i] + " ";
+        }
+        SecureRandom random = new SecureRandom();
+        int enemyMove = random.nextInt(arguments.length);
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(random);
+        SecretKey secretKey = keyGen.generateKey();
+        String secrKey = new BigInteger(1, secretKey.getEncoded()).toString(16);
+        try {
+            byte[] hmacSha256 = game.calcHmacSha256(secrKey.getBytes("UTF-8"), se.getBytes("UTF-8"));
+            System.out.println(String.format("HMAC: %032x", new BigInteger(1, hmacSha256)).toUpperCase(Locale.ROOT));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Available moves:");
         for(int i = 0; i < arguments.length; i++) {
             System.out.println((i+1) +" - " +arguments[i]);
         }
         System.out.print("0 - exit\n? - help\nEnter your move: ");
         Scanner in = new Scanner(System.in);
-        char move = in.nextLine().charAt(0);
-        Reaction(move, arguments);
+        String mv = in.nextLine();
+        char move;
+        if(mv.isEmpty()){
+            System.out.println("Action not selected!");
+            Menu(arguments);
+        } else {
+            move = mv.charAt(0);
+            Reaction(move, arguments, secrKey, enemyMove);
+        }
     }
 
-    public static void Reaction(char move, String[] arguments) throws NoSuchAlgorithmException, IOException {
+    public static void Reaction(char move, String[] arguments, String key, int enemyMove) throws NoSuchAlgorithmException, IOException {
         if(Character.isDigit(move))  {
             int move_ = move - '0';
             if(move_ >= 1 & move_ < arguments.length + 1) {
-                Game((move_ - 1), arguments);
+                Game((move_ - 1), arguments, enemyMove, key);
             }
-
             if(move_ == 0) { System.out.println("End of the game!"); System.exit(0); }
         }
         if(move == '?') { AsciiPrint(arguments); }
         Menu(arguments);
     }
 
-    public static void Game(int playerMove, String[] arguments) throws NoSuchAlgorithmException {
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        SecureRandom random = new SecureRandom();
-        keyGen.init(random);
-        SecretKey secretKey = keyGen.generateKey();
-        String s = new BigInteger(1, secretKey.getEncoded()).toString(16);
-
-        int EnemyMove = random.nextInt(arguments.length);
+    public static void Game(int playerMove, String[] arguments, int enemyMove, String key) throws NoSuchAlgorithmException {
         String prediction = null;
         int situatuinCase = -1, LenD = arguments.length / 2;
-        if(playerMove == EnemyMove) { situatuinCase = 0; }
-        else { if(playerMove > EnemyMove) { situatuinCase = 1; }  else  { situatuinCase = 2; } }
+        if(playerMove == enemyMove) { situatuinCase = 0; }
+        else { if(playerMove > enemyMove) { situatuinCase = 1; }  else  { situatuinCase = 2; } }
         switch (situatuinCase) {
             case 0: { prediction = "Draw!"; break; }
             case 1: {
@@ -83,10 +86,10 @@ public class game {
                     int a;
                     if(playerMove + LenD == arguments.length) { a = 0; }
                     else { a = (playerMove + LenD) - arguments.length; }
-                    if(a >= EnemyMove) { prediction = "You lose!"; }
+                    if(a >= enemyMove) { prediction = "You lose!"; }
                     else { prediction = "You win!"; }
                 }  else {
-                    if(playerMove < EnemyMove) { prediction = "You lose!"; }
+                    if(playerMove < enemyMove) { prediction = "You lose!"; }
                     else { prediction = "You win!"; }
                 }
                 break;
@@ -96,16 +99,16 @@ public class game {
                     int a;
                     if(playerMove - LenD > 0) { a = arguments.length - 1; }
                     else { a = (playerMove - LenD) + arguments.length; }
-                    if(a <= EnemyMove) { prediction = "You win!"; }
+                    if(a <= enemyMove) { prediction = "You win!"; }
                     else { prediction = "You lose!"; }
                 }  else {
-                    if(playerMove > EnemyMove) { prediction = "You win!"; }
+                    if(playerMove > enemyMove) { prediction = "You win!"; }
                     else { prediction = "You lose!"; }
                 }
                 break;
             }
         }
-        System.out.println("Your move: " +arguments[playerMove] +"\nComputer move: " +arguments[EnemyMove] +"\n" +prediction +"\nKEY: "+s.toUpperCase(Locale.ROOT));
+        System.out.println("Your move: " +arguments[playerMove] +"\nComputer move: " +arguments[enemyMove] +"\n" +prediction +"\nHMAC KEY: "+key.toString().toUpperCase(Locale.ROOT) +"\n");
     }
 
     public static void AsciiPrint(String[] arguments){
